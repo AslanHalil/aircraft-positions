c920701fced64a3b3927ea62232b91269feac0e7
Aslan <AslanCHalil@outlook.com>
2021-03-23 13:58:11 +0200

    First commit

---
+++ src/test/java/com/example/aircraftpositions/AircraftpositionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aircraftpositions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftpositionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
